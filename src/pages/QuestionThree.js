e330ab1806ef925695cab00d8666b87423bc4895
bder00s <bonne-deroos@hotmail.com>
2022-11-01 19:24:31 +0100

    Vragen 1-3 gemaakt + navigatiebuttons + state

--- src/pages/QuestionThree.js
+++ src/pages/QuestionThree.js
@@ -1,12 +1,61 @@
-import React from "react"
+import React, {useState} from "react"
+import Answer from "../Components/Answer";
+import {Link, useHistory} from "react-router-dom";
+import NavButton from "../Components/NavButton";
 
 function QuestionThree() {
+    const history = useHistory();
+    const [changeMood, toggleChangeMood] = useState(false);
+    const [keepMood, toggleKeepMood] = useState(false);
+
+    function handleSubmitThree(e) {
+        e.preventDefault()
+        console.log(`Vraag 3 beantwoord.
+        Wil mood veranderen: ${changeMood},
+        Wil mood niet veranderen: ${keepMood}
+        `)
+        history.push("/result")
+    }
+
     return (
         <div className="question-three">
             <header>
-
-
             </header>
+            <h3>Vraag 3</h3>
+            <p>Wil je hier verandering in brengen?</p>
+
+            {/*//ANTWOORDEN*/}
+
+            <form className="question-three" onSubmit={handleSubmitThree}>
+                <Answer
+                answerId="yes"
+                answerName="question-three"
+                answerValue={changeMood}
+                setAnswerValue={toggleChangeMood}
+                answerContent="Ja"
+                />
+                <Answer
+                answerId="no"
+                answerName="question-three"
+                answerValue={keepMood}
+                setAnswerValue={toggleKeepMood}
+                answerContent="Nee"
+                />
+
+                <Link to="/question-two">
+                    <NavButton
+                        buttonType="button"
+                        buttonText="Vorige"
+
+                    />
+                </Link>
+
+                <NavButton buttonType="submit"
+                           buttonText="Geef me een film!"
+                           answerValues={changeMood || keepMood}
+                />
+
+            </form>
         </div>
     );
 }
--- src/pages/QuestionTwo.js
+++ src/pages/QuestionTwo.js
@@ -1,12 +1,99 @@
-import React from "react"
+import React, {useState} from "react"
+import Answer from "../Components/Answer";
+import {Link, useHistory} from "react-router-dom";
+import NavButton from "../Components/NavButton";
 
 function QuestionTwo() {
+    const history = useHistory();
+
+    const [checkedGreat, toggleCheckedGreat] = useState(false);
+    const [checkedLoved, toggleCheckedLoved] = useState(false);
+    const [checkedShitty, toggleCheckedShitty] = useState(false);
+    const [checkedAngry, toggleCheckedAngry] = useState(false);
+    const [checkedBored, toggleCheckedBored] = useState(false);
+    const [checkedSad, toggleCheckedSad] = useState(false);
+
+
+    function handleSubmitTwo(e) {
+        e.preventDefault()
+        console.log(`Vraag 2 beantwoord.
+        Ik voel me goed: ${checkedGreat},
+        Ik voel me geliefd: ${checkedLoved},
+        Ik voel me slecht: ${checkedShitty},
+        Ik voel me boos: ${checkedAngry},
+        Ik voel me verveeld: ${checkedBored},
+        Ik voel me verdrietig: ${checkedSad}
+        `)
+        history.push("/question-three")
+    }
     return (
         <div className="question-two">
             <header>
-
-
             </header>
+            <h3>Vraag 2</h3>
+            <p>Hoe voel je je?</p>
+
+            {/*//ANTWOORDEN*/}
+
+            <form className="question-two" onSubmit={handleSubmitTwo}>
+                <Answer
+                    answerId="great"
+                    answerName="answer-question-two"
+                    answerValue={checkedGreat}
+                    setAnswerValue={toggleCheckedGreat}
+                    answerContent="Goed"
+
+                />
+                <Answer
+                    answerId="loved"
+                    answerName="answer-question-two"
+                    answerValue={checkedLoved}
+                    setAnswerValue={toggleCheckedLoved}
+                    answerContent="Geliefd"
+                />
+                <Answer
+                    answerId="shitty"
+                    answerName="answer-question-two"
+                    answerValue={checkedShitty}
+                    setAnswerValue={toggleCheckedShitty}
+                    answerContent="Slecht"
+                />
+                <Answer
+                answerId="angry"
+                answerName="answer-question-two"
+                answerValue={checkedAngry}
+                setAnswerValue={toggleCheckedAngry}
+                answerContent="Boos"
+                />
+                <Answer
+                answerId="bored"
+                answerName="answer-question-two"
+                answerValue={checkedBored}
+                setAnswerValue={toggleCheckedBored}
+                answerContent="Verveeld"
+                />
+                <Answer
+                answerId="sad"
+                answerName="answer-question-two"
+                answerValue={checkedSad}
+                setAnswerValue={toggleCheckedSad}
+                answerContent="Verdrietig"
+                />
+
+                {/*//NAVIGATIE BUTTONS*/}
+
+                <Link to="/question-one">
+                    <NavButton
+                        buttonType="button"
+                        buttonText="Vorige"
+                    />
+                </Link>
+
+                <NavButton buttonType="submit"
+                           buttonText="Volgende"
+                answerValues={checkedGreat || checkedLoved || checkedShitty || checkedAngry || checkedBored || checkedSad}/>
+
+            </form>
         </div>
     );
 }
